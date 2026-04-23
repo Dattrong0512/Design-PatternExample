975ce4ecd82f2cdf2e5498cc9e514bc47d340e0c
Trong Le <trongle@ankvn.com>
2026-04-23 14:58:19 +0700

    Add Abstract Factory and Factory Method Design Pattern

---
+++ Abstract Factory/Program.cs
@@ -0,0 +1,99 @@
+﻿IStyle modernStyle = new ModernStyle();
+Furniture modernFurniture = new Furniture(modernStyle);
+modernFurniture.Init();
+
+IStyle classicStyle = new ClassicStyle();
+Furniture classicFurniture = new Furniture(classicStyle);
+classicFurniture.Init();
+
+public interface IChair
+{
+    void Init();
+}
+
+public interface ITable
+{
+    void Init();
+}
+
+public class ClassicChair : IChair
+{
+    public void Init()
+    {
+        Console.WriteLine($"Create successful {typeof(ClassicChair)}");
+    }
+}
+
+public class ModernChair : IChair
+{
+    public void Init()
+    {
+        Console.WriteLine($"Create successful {typeof(ModernChair)}");
+    }
+}
+
+public class ClassicTable : ITable
+{
+    public void Init()
+    {
+        Console.WriteLine($"Create successful {typeof(ClassicTable)}");
+    }
+}
+
+public class ModernTable : ITable
+{
+    public void Init()
+    {
+        Console.WriteLine($"Create successful {typeof(ModernTable)}");
+    }
+}
+
+public interface IStyle
+{
+    IChair CreateChair();
+    ITable CreateTable();
+}
+
+public class ClassicStyle : IStyle
+{
+    public IChair CreateChair()
+    {
+        return new ClassicChair();
+    }
+
+    public ITable CreateTable()
+    {
+        return new ClassicTable();
+    }
+}
+
+public class ModernStyle : IStyle
+{
+    public IChair CreateChair()
+    {
+        return new ModernChair();
+    }
+
+    public ITable CreateTable()
+    {
+        return new ModernTable();
+    }
+}
+
+public class Furniture
+{
+    private IChair chair;
+    private ITable table;
+
+    public Furniture(IStyle style)
+    {
+        chair = style.CreateChair();
+        table = style.CreateTable();
+    }
+
+    public void Init()
+    {
+        chair.Init();
+        table.Init();
+    }
+}
---
+++ Factory Method/Program.cs
@@ -0,0 +1,48 @@
+﻿
+LogisticBase roadLogistic = new RoadLogistic();
+roadLogistic.DeliveryByTransport();
+LogisticBase waterLogistic = new WaterWaysLogistic();
+waterLogistic.DeliveryByTransport();
+
+
+public interface ITransport
+{
+    void Transport();
+}
+
+public class Ship : ITransport
+{
+    public void Transport() => Console.WriteLine("Ship are transporting");
+}
+
+public class Truck : ITransport
+{
+    public void Transport() => Console.WriteLine("Truck are transporting");
+}
+
+public abstract class LogisticBase
+{
+    public abstract ITransport CreateTransport();
+    public void DeliveryByTransport()
+    {
+        ITransport transport = CreateTransport();
+        Console.WriteLine($"Transport by {this.GetType().Name}");
+        transport.Transport();
+    }
+}
+
+public class RoadLogistic : LogisticBase
+{
+    public override ITransport CreateTransport()
+    {
+        return new Truck();
+    }
+}
+
+public class WaterWaysLogistic : LogisticBase
+{
+    public override ITransport CreateTransport()
+    {
+        return new Ship();
+    }
+}
